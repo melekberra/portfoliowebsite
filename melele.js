a811610eb97fae04bc9fb7c46003cdda017b672f
melekberra <melekberraaksu@gmail.com>
2026-03-31 16:01:11 +0300

    Add files via upload

---
+++ melele.js
@@ -0,0 +1,53 @@
+let example = 20;
+
+let x;
+
+let my_cat;
+
+x = 30;
+
+example + my_cat
+
+console . log(example)
+
+console . log(x)
+
+my_cat = 50
+
+console . log(my_cat)
+
+const coding = 50
+
+console . log(coding)
+
+let greetings = "Hello"
+
+let name = "Alice"
+
+let space = " "
+
+let mario = "87"
+
+let superb = "54"
+
+let supermario = superb + mario
+
+console . log(supermario)
+
+let isStudent = true;
+
+let hasJob = false;
+
+console .log(example+my_cat)
+
+let total = example + my_cat
+
+console . log(total)
+
+let h = greetings + space + name 
+
+console . log(h)
+
+mario  = 30
+
+console . log (mario)
